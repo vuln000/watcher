54ba57ae7819750bf2f849ae8e5e23812ee4819d
sunxueliang96 <sundianzhang@icloud.com>
2019-12-08 11:32:30 -0500

    update protocol analysis, Beautification interface

--- analysis.py
+++ analysis.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import matplotlib.pylab as plt
 import time
-
+email_addr = '237834893@qq.com'
+alpha = 1000
 plt.ion()
 #fig, ax = plt.subplots(3, 1)
 file = 'a.txt'
@@ -9,40 +10,75 @@ def plot_stream(file):
     plt.clf()
 
     data =pd.read_csv('a.txt')
-    data.columns=['time','ip_src','port_src','ip_dst','port_dst','porto','n']
+    data.columns=['time','ip_src','port_src','ip_dst','port_dst','protocol','n']
     del data['n']
 
     #io graph start
     io = data['time'].value_counts(sort=False,ascending=True)
+    if(sum(io[-5:-1])>alpha):        #whilee sum of the last five seconds exceed alpha,send a alert email or smb message
+        try:
+            send_alert_email(sum(io[-5:-1]))
+        except:
+            send_alert_smb(sum(io[-5:-1]))
     io = io.sort_index(ascending=True)
     graph_io = plt.subplot(3, 2, 1)
     graph_io.set_xlabel('time',fontsize=10)
-    graph_io.set_ylabel('number', fontsize=10)
+    graph_io.set_ylabel('packet per seconds', fontsize=10)
     graph_io.plot(io)
+
+    graph_io.spines['top'].set_visible(False)
+    graph_io.spines['right'].set_visible(False)
+    graph_io.spines['bottom'].set_visible(False)
+    graph_io.spines['left'].set_visible(False)
+
     ##print io
     #io graph end
     sent = data['ip_src'].value_counts()[0:5] #user sent
     graph_sent = plt.subplot(3, 2, 3)
     graph_sent.set_xlabel('user_ip',fontsize=10)
     graph_sent.set_ylabel('sent_number', fontsize=10)
-    graph_sent.bar(sent.index,sent)
+    graph_sent.bar(sent.index,sent,color='c')
+    graph_sent.spines['top'].set_visible(False)
+    graph_sent.spines['right'].set_visible(False)
+    graph_sent.spines['bottom'].set_visible(False)
+    graph_sent.spines['left'].set_visible(False)
 
     recv = data['ip_dst'].value_counts()[0:5] #user_recv
     graph_recv = plt.subplot(3, 2, 5)
     graph_recv.set_xlabel('user_ip',fontsize=10)
     graph_recv.set_ylabel('recv_number', fontsize=10)
-    graph_recv.bar(sent.index,recv)
+    graph_recv.bar(sent.index,recv,color='g')
+    graph_recv.spines['top'].set_visible(False)
+    graph_recv.spines['right'].set_visible(False)
+    graph_recv.spines['bottom'].set_visible(False)
+    graph_recv.spines['left'].set_visible(False)
 
-    port_src = data['port_src'].value_counts()[0:10]
+    port_src = data['port_src'].value_counts()[0:7]#port_src_pie
     graph_port_src = plt.subplot(2,2,2)
-    graph_port_src.pie(port_src)
+    graph_port_src.pie(port_src,labels=port_src.index,autopct='%1.1f%%')
 
-    port_dst = data['port_dst'].value_counts()[0:10]
+    protocol = data['protocol'].value_counts()[0:7]#protocal_dst_pie
     graph_port_dst = plt.subplot(2,2,4)
-    graph_port_dst.pie(port_dst)
+    graph_port_dst.pie(protocol,labels=protocol.index,autopct='%1.1f%%')
+
     #plt.draw()
     plt.pause(0.1)
-
+def send_alert_email():
+    try:
+        #send..email..here
+        print "send"
+        return True
+    except:
+        print"send alert email failed try to send smb message"
+        return False
+def send_alert_smb():
+    try:
+        #send..messagge..here
+        print "send"
+        return True
+    except:
+        print"send alert email failed try to send smb message"
+        return False
 while True:
     plot_stream(file)
     #time.sleep(3)
--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
 from scapy.all import *
-
+import socket
 file = 'a.txt'
 def write_to_txt(data):
     with open(file,'a+') as f:
@@ -11,26 +11,36 @@ def parse_ip(pkt):
     temp = []
     if pkt.haslayer('IP'):#ipv4 for now
         if pkt.haslayer('TCP'):
-            print 'tcp'
-            print str(pkt[IP].src) + ':' + str(pkt[TCP].sport) + '---->' + str(pkt[IP].dst) + ':' + str(pkt[TCP].dport)
+            #print 'tcp'
+            #print str(pkt[IP].src) + ':' + str(pkt[TCP].sport) + '---->' + str(pkt[IP].dst) + ':' + str(pkt[TCP].dport)
             temp.append(int(time.time()%100000))
             temp.append(str(pkt[IP].src))
             temp.append(str(pkt[TCP].sport))
             temp.append(str(pkt[IP].dst))
             temp.append(str(pkt[TCP].dport))
-            temp.append('TCP')
+            #temp.append('TCP')
+            temp.append(get_servers_by_port(pkt[TCP].sport,pkt[TCP].dport))
+            print 'tcp   ' + str(pkt[IP].src) + ':' + str(pkt[TCP].sport) + '---->' + str(pkt[IP].dst) + ':' + str(pkt[TCP].dport) + '   ' +str(temp[-1])
 
         if pkt.haslayer('UDP'):
-            print 'udp'
-            print str(pkt[IP].src) + ':' + str(pkt[UDP].sport) + '---->' + str(pkt[IP].dst) + ':' + str(pkt[UDP].dport)
+            #print 'udp'
             temp.append(int(time.time()%100000))
             temp.append(str(pkt[IP].src))
             temp.append(str(pkt[UDP].sport))
             temp.append(str(pkt[IP].dst))
             temp.append(str(pkt[UDP].dport))
-            temp.append('UDP')
+            #temp.append('UDP')
+            temp.append(get_servers_by_port(pkt[UDP].sport,pkt[UDP].dport))
+            print 'udp   ' + str(pkt[IP].src) + ':' + str(pkt[UDP].sport) + '---->' + str(pkt[IP].dst) + ':' + str(pkt[UDP].dport) + '   ' +str(temp[-1])
+
         if(len(temp)>0):
             write_to_txt(temp)
+def get_servers_by_port(sport,dport):#find the servers by port# only concern application ios model for now
+    try:
+        servers = socket.getservbyport(min(sport,dport))
+    except:
+        return 'Unknown'
+    return servers
 
 def watcher(interface):
     sniff(iface=interface, prn=parse_ip)
